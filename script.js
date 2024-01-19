e0682557ef48d51acf40d1bacd68a2afdd6c243a
JovanioOliveira <135073526+JovanioOliveira@users.noreply.github.com>
2024-01-19 19:45:00 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const apiKey = '790af7bc'; // Apenas a chave da API, não a URL completa
+
+function searchMovie() {
+    const movieInput = document.getElementById('movieInput').value;
+
+    /*fetch: Usa para fazer uma requisição do recurso do JSON de servidores*/
+    /*encodeURIComponent: Trata caracteres especiais da string que será incluída na URL*/
+    fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(movieInput)}&apikey=${apiKey}`)
+        /*.then(response..)..: É uma chamada de API fetch para processar resposta HTTP*/
+        /*.then: Manipula o resultado assincrono*/
+        /*.resonse.json.: Retorno da chamada que tá sendo passada no .then*/
+        
+        .then(response => response.json())
+        .then(data => {
+            const resultContainer = document.getElementById('movieResult');
+            const sadFace = document.getElementById('sadFace');
+            const errorMessage = document.getElementById('errorMessage');
+            const movieDetails = document.getElementById('movieDetails');
+
+            if (data.Response === 'True') {
+                sadFace.style.display = 'none';
+                errorMessage.textContent = '';
+                displayMovieDetails(data, movieDetails);
+            } else {
+                sadFace.style.display = 'block';
+                errorMessage.textContent = 'Filme não encontrado.';
+                movieDetails.style.display = 'none';
+            }
+
+            resultContainer.style.display = 'block';
+        })
+        .catch(error => console.error('Erro na busca do filme:', error));
+}
+
+function displayMovieDetails(data, container) {
+    container.style.display = 'block';
+    container.innerHTML = `
+        <img src="${data.Poster}" alt="${data.Title}">
+        <h2>${data.Title}</h2>
+        <p><strong>Ano:</strong> ${data.Year}</p>
+        <p><strong>Gênero:</strong> ${data.Genre}</p>
+        <p><strong>Duração:</strong> ${data.Runtime}</p>
+        <p><strong>Sinopse:</strong> ${data.Plot}</p>
+        <!--<p><strong>Diretor:</strong> ${data.Director}</p>-->
+        <!--<p><strong>Atores:</strong> ${data.Actors}</p>-->       
+        
+    `;
+}
+
